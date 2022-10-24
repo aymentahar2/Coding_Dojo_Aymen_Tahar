52ed867a9f587dd9cd4e70cb18c05057e5c72904
aymentahar2 <110730241+aymentahar2@users.noreply.github.com>
2022-10-24 14:37:22 +0100

    update

---
+++ Web-fun/Core-assignment/week2/day1/Button Clicker/script.js
@@ -0,0 +1,17 @@
+
+
+function ninja() {
+    alert("ninja was liked!") ; 
+}
+
+function login(element) {
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(element) {
+    element.remove() ;
+}
