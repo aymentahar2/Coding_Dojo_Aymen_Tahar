d633333ee030b066ce9d9148e8df6d945ff76ec4
aymentahar2 <110730241+aymentahar2@users.noreply.github.com>
2022-10-24 15:54:56 +0100

    Hello_World

---
+++ stacks/Python-Flask/Week1/Day1/practice-assignment/Hello_world.py
@@ -0,0 +1,20 @@
+# print("hello world")
+
+# name="Aymen"
+# # print("hello",name)
+
+# # print("hello "+ name)
+
+# Num="45"
+# # print("hello",Num)
+
+# print("hello "+ Num)
+
+# print("i love to eat {food_one} and {food_two}".format(food_one= "kosksi", food_two = "lablabi"))
+
+
+food_one="kosksi"
+food_two="lablabi"
+
+print(f"i love to eat {food_one} and {food_two}")
+
