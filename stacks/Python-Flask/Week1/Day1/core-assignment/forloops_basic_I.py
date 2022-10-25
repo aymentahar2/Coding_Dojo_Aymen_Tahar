1f32383cae51847946b2b05250f98cd1574e2e02
aymentahar2 <110730241+aymentahar2@users.noreply.github.com>
2022-10-25 16:14:16 +0100

    update

--- stacks/Python-Flask/Week1/Day1/core-assignment/forloops_basic_I.py
+++ stacks/Python-Flask/Week1/Day1/core-assignment/forloops_basic_I.py
@@ -1,38 +1,41 @@
-# all integers from 0 to 150.
-for i in range(0, 151):
-    print(i)
+# # all integers from 0 to 150.
+# for i in range(0, 151):
+#     print(i)
 
 
 
-#all the multiples of 5 from 5 to 1,000
-for i in range(5,1001,5):
-    print(i)
+# #all the multiples of 5 from 5 to 1,000
+# for i in range(5,1001,5):
+#     print(i)
 
-#Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
-for i in range(0 ,101):
-    if i % 10 == 0:
-        print("Coding Dojo")
-    elif i % 5 == 0:
-        print("Coding")
-    else:
-        print(i)
+# #Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+# for i in range(0 ,101):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#     elif i % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(i)
 
-#the final sum.
-sum = 0
-for i in range(1,500001,2):
-    sum += i
-print(sum)
+# #the final sum.
+# sum = 0
+# for i in range(1,500001,2):
+#     sum += i
+# print(sum)
 
-#Print positive numbers starting at 2018, counting down by fours.
-for i in range(2018,0,-4):
-    print(i)
+# #Print positive numbers starting at 2018, counting down by fours.
+# for i in range(2018,0,-4):
+#     print(i)
 
-# # 6. Flexible Counter 
+# # # 6. Flexible Counter 
 
-low = 2
-high = 9
-mult = 3
+# low = 2
+# high = 9
+# mult = 3
 
-for i in range(low,high + 1):
-    if i % mult == 0:
-        print(i)
+# for i in range(low,high + 1):
+#     if i % mult == 0:
+#         print(i)
+y = "dog"
+for x in y:
+    print(x)
