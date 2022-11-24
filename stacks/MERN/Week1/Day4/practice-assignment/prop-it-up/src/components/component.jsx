bda9f4e1964128358bce71d89e4994c46b439c47
aymentahar2 <110730241+aymentahar2@users.noreply.github.com>
2022-11-24 15:34:06 +0100

    first commit

---
+++ stacks/MERN/Week1/Day4/practice-assignment/prop-it-up/src/components/component.jsx
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react'
+import classe from './component.css'
+class PersonCard extends Component {
+
+    render() {
+        return (
+            <div class='comp'>
+            <h1>
+              {this.props.firstName},{this.props.lastName}
+              </h1>
+              <p> Age : {this.props.age}</p>
+              <p> Hair Color : {this.props.hairColor}</p>
+            </div>  
+        )
+    }
+}
+
+export default PersonCard ;   
