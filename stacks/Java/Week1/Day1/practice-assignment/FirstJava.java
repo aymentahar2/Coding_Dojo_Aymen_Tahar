d9faa4b1420b8045f33cac3805eaa10b07494eec
aymentahar2 <110730241+aymentahar2@users.noreply.github.com>
2022-12-20 22:52:37 +0100

    cafeJava

--- stacks/Java/Week1/Day1/practice-assignment/FirstJava.java
+++
@@ -1,7 +0,0 @@
-public class FirstJava {
-    public static void main ( String[] args) {
-        System.out.println("My name is Aymen Tahar");
-        System.out.println("I'm 26 YO");
-        System.out.println("MY homeTown is beng");
-    }
-}
