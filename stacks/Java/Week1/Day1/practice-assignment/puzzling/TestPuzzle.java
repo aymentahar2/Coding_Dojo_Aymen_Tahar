9934ed7690f5dcfdbe6bdae2cdfa86af6e90bca6
aymentahar2 <110730241+aymentahar2@users.noreply.github.com>
2022-12-25 14:14:31 +0100

    Pussle

---
+++ stacks/Java/Week1/Day1/practice-assignment/puzzling/TestPuzzle.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+// import java.util.Random;
+public class TestPuzzle {
+    
+	public static void main(String[] args) {
+		Puzzle generator = new Puzzle();
+		ArrayList<Integer> randomRolls = generator.getTenRolls();
+		System.out.println(randomRolls);
+
+        
+    // getRandomLetter
+        String randomLetter = generator.getRandomLetter();
+        System.out.println(randomLetter);
+     // generatePassword
+     System.out.println(generator.generatePassword());
+	 // getNewPasswordSet
+     System.out.println(generator.getNewPasswordSet(20));
+    	
+	}
+}
